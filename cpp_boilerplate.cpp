327ff475baec866613394fd7d30c8ae3eebe26b9
PRERNA MISHRA <mprerna802@gmail.com>
2020-11-24 16:44:48 +0530

    Create cpp_boilerplate.cpp

---
+++ cpp_boilerplate.cpp
@@ -0,0 +1,16 @@
+{
+	//paste this in your cpp.json (file under file->preferences->user snippets->cpp.json)
+	"cpp snippets": {
+		"prefix": "boilerplate",
+		"body": [
+			"#include<iostream>",
+			"using namespace std;",
+			"int main()",
+			"{",
+			"    return 0;",
+			"}"
+			
+		],
+		"description": "to produce the main snippet for cpp"
+	}
+}
